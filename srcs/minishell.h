aa7e71dc5be8e24a7b2caa978f8a3407358c3b43
Adrien DA SILVA FERNANDES <adda-sil@student.42.fr>
2020-02-01 20:38:51 +0100

    Setup deps

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -1,19 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/01 20:27:15 by adda-sil          #+#    #+#             */
-/*   Updated: 2020/02/01 20:35:38 by adda-sil         ###   ########.fr       */
+/*   Created: 2020/02/01 20:32:31 by adda-sil          #+#    #+#             */
+/*   Updated: 2020/02/01 20:35:49 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-int main(void)
-{
-	ft_printf("Oy\n");
-	return (0);
-}
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "libft.h"
+# include "get_next_line.h"
+# include "ft_printf.h"
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdio.h>
+#endif
