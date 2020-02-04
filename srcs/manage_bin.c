e37516e575ac8a52e94ff320571910d545d5d51f
Richard <richard.blc25@gmail.com>
2020-02-04 09:10:26 +0100

    mv manage_bin.c.bak to manage_bin.c

--- srcs/manage_bin.c
+++ srcs/manage_bin.c
@@ -6,62 +6,105 @@
 /*   By: riblanc <riblanc@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 01:56:11 by riblanc           #+#    #+#             */
-/*   Updated: 2020/02/04 04:21:38 by riblanc          ###   ########.fr       */
+/*   Updated: 2020/02/04 09:09:50 by riblanc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <sys/types.h>
-
+#include <dirent.h>
 
 int		try_exec(char *path, char **argv, char **envp, pid_t child)
 {
 	int		status;
 	int		ret;
+	pid_t	pid;
 
 	if (child == -1)
-		return -1; //Failed
+		return (-1);
 	if (child > 0)
 	{
-		/* I am the parent - wait for the child to finish */
-		pid_t pid = waitpid(child, &status, 0);
+		pid = waitpid(child, &status, 0);
 		printf("status: %d\n", status);
 		return (status);
 	}
 	else
-	{ /* I am the child */
-		// do something interesting
-		ret = execve(path, argv, envp); // "ls ."
+	{
+		ret = execve(path, argv, envp);
 		return (ret);
 	}
 	return (-1);
+}
+
+int		test_dir(char *path, char *cmd)
+{
+	struct dirent	*file;
+	DIR				*rep;
+
+	file = NULL;
+	rep = NULL;
+	if ((rep = opendir(path)) == NULL)
+		return (-1);
+	while ((file = readdir(rep)) != NULL)
+	{
+		if (!ft_strncmp(file->d_name, cmd, ft_strlen(cmd) + 1))
+		{
+			if (closedir(rep) == -1)
+				return (-1);
+			return (1);
+		}
+	}
+	if (closedir(rep) == -1)
+		return (-1);
+	return (0);
+}
+
+int		fork_exec(t_list *lst_env, t_cmd *cmd, char *tmp[2])
+{
+	pid_t	child;
+	char	**envp;
+	char	*bin_path;
+	char	*res;
+	int		ret;
+
+	child = -1;
+	envp = convert_env_list(lst_env);
+	child = fork();
+	bin_path = ft_strmjoin(2, tmp, "/");
+	ret = try_exec(bin_path, cmd->argv, envp, child);
+	if (child == 0 && ret != 0)
+	{
+		exit(ret);
+		return (ret);
+	}
+	free(bin_path);
+	free_env_array(envp);
+	return (0);
 }
 
 int		exec_bin(t_list *lst_env, t_cmd *cmd)
 {
 	char	**paths;
 	char	*tmp[2];
-	char	*res;
+	int		i;
 	int		ret;
-	pid_t child = -1;
 
 	paths = ft_split(get_value(lst_env, "PATH"), ':');
-	int		i = -1;
 	tmp[1] = cmd->argv[0];
-	child = fork();
+	i = -1;
 	while (paths[++i])
 	{
 		tmp[0] = paths[i];
-		res = ft_strmjoin(2, tmp, "/");
-		if (!(ret = try_exec(res, cmd->argv, convert_env_list(lst_env), child)))
+		ret = test_dir(tmp[0], tmp[1]);
+		if (ret == 1)
 			break ;
 	}
-	if (child == 0 && ret == -1)
+	if (ret != 1)
 	{
-		exit(ret);
-		return (ret);
+		free_env_array(paths);
+		return (0);
 	}
-	else if (ret == -1)
-		printf("pas trouve\n");
-	return (ret != -1);
+	fork_exec(lst_env, cmd, tmp);
+	free_env_array(paths);
+	return (1);
 }
